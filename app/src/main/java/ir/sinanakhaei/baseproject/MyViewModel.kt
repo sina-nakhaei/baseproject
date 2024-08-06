c441a989b531098df3f211c6bf5e4ce7c7ff0fdf
Sina Nakhaei <cna.nakhaei@gmail.com>
2024-08-06 20:19:44 +0330

    fix compile time error

--- app/src/main/java/ir/sinanakhaei/baseproject/MyViewModel.kt
+++ app/src/main/java/ir/sinanakhaei/baseproject/MyViewModel.kt
@@ -1,17 +1,13 @@
 package ir.sinanakhaei.baseproject
 
 import android.util.Log
-import android.util.Log.d
-import androidx.compose.ui.tooling.data.EmptyGroup.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.skydoves.sandwich.map
 import com.skydoves.sandwich.message
 import com.skydoves.sandwich.onError
 import com.skydoves.sandwich.onSuccess
 import com.skydoves.sandwich.retrofit.statusCode
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.internal.NopCollector.emit
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,17 +18,12 @@ class MyViewModel @Inject constructor(
     fun getPost() {
         viewModelScope.launch {
             val response = myRepo.getPosts()
-            response.onSuccess {
-                Log.d("sinatest", "getPost: $data")
-            }.onError {
+            response.onSuccess {}.onError {
                 statusCode
                 message()
             }
             
-            response.onError {
-                Log.d("sinatest", "getPost: $code")
-            }
-            
+            response.onError {}
         }
     }
 }
